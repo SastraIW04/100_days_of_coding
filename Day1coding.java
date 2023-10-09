a047e04260a8b37293aaaa053d9ada5f32c77f92
SastraIW04 <147222527+SastraIW04@users.noreply.github.com>
2023-10-09 20:22:29 +0800

    Day1coding.java
    package tipedata1 ;
    
    public class Tipedata {
        
        public static void main(String[] args) {
            String nama = "Sastra";
            String kelas = "Inf C";
            int angkatan = 23;
            
            System.out.println(nama);
            System.out.println(kelas);
            System.out.println(angkatan);
            
            
            
        }
        
    }

---
+++ Day1coding.java
@@ -0,0 +1,18 @@
+package tipedata1 ;
+
+public class Tipedata {
+    
+    public static void main(String[] args) {
+        String nama = "Sastra";
+        String kelas = "Inf C";
+        int angkatan = 23;
+        
+        System.out.println(nama);
+        System.out.println(kelas);
+        System.out.println(angkatan);
+        
+        
+        
+    }
+    
+}
