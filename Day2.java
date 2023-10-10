de60c58cb6458a13c8ee2be6c4a9c65ca0c30d3b
SastraIW04 <147222527+SastraIW04@users.noreply.github.com>
2023-10-11 00:39:49 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,12 @@
+package day2 ;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        short umur = 18;
+        String keterangan = "Tahun enam bulan";
+        
+        System.out.println(umur+keterangan);
+    }
+    
+}
