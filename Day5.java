b95d6b37c6c3e52b86a75042d43049564cc4690e
SastraIW04 <147222527+SastraIW04@users.noreply.github.com>
2023-10-14 23:11:55 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+package Day5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int ukuranTinggi = 171;
+        if (ukuranTinggi >= 170) {
+            boolean tinggi = true;
+            System.out.println("tinggi");
+        }else {
+            boolean tinggi = false;
+            System.out.println(tinggi);
+        }
+    }
+    
+}
