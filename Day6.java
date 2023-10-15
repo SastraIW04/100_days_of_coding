ecda7b072051c73553e1e9ff54cbc9b03a476fae
SastraIW04 <147222527+SastraIW04@users.noreply.github.com>
2023-10-15 23:12:41 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+package Day6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int hasil;
+        
+        hasil = a+b;
+        System.out.println("nilai dari A = "+ a);
+        System.out.println("nilai dari B = "+ b);
+        System.out.println("hasil penjumlahan adalah = " +hasil);
+        
+    }
+    
+}
