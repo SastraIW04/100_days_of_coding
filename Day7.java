c122a114144078a66ea94bd9be622a0fa0c49036
SastraIW04 <147222527+SastraIW04@users.noreply.github.com>
2023-10-16 22:40:28 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,11 @@
+package Day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        double phi = 3.14;
+        System.out.println("nilai phi = "+phi);
+        
+    }
+    
+}
